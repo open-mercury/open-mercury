0ca3247e580e31fc46683294fe0c246449428426
Justin Meiners <justin.meiners@gmail.com>
2018-06-15 21:39:01 -0600

    added server package

---
+++ mercury-server/index.js
@@ -0,0 +1,33 @@
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({
+    port: 8080,
+    perMessageDeflate: {
+        zlibDeflateOptions: {
+            chunkSize: 1024,
+            memLevel: 7,
+            level: 3,
+        },
+        zlibInflateOptions: {
+            chunkSize: 10 * 1024
+        },
+
+        clientNoContextTakeover: true,
+        serverNoContextTakeover: true,
+        clientMaxWindowBits: 10,
+        serverMaxWindowBits: 10,
+
+        concurrencyLimit: 10,
+        threshold: 1024,
+    }
+});
+
+wss.on('connection', function connection(ws) {
+    ws.on('message', function incoming(message) {
+        console.log('received: %s', message);
+    });
+
+    ws.send('something');
+});
+
+
