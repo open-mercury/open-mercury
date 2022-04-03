2598b75a08a208d9a46b10256ed9e530b665816f
Ryan Pendleton <me@ryanp.me>
2022-04-03 00:48:40 -0600

    update to latest React

--- mercury-client/src/index.tsx
+++ mercury-client/src/index.tsx
@@ -1,11 +1,9 @@
 import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
+
 import App from './App';
 import './index.css';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root') as HTMLElement
-);
-registerServiceWorker();
+const container = document.getElementById('root')!;
+const root = createRoot(container);
+root.render(<App />);
