24610f01e054e99fabe0305b9c183f3e61a7dfcc
Ryan Pendleton <me@ryanp.me>
2022-04-03 01:16:28 -0600

    switch to react hooks

--- mercury-client/src/App.tsx
+++ mercury-client/src/App.tsx
@@ -1,4 +1,6 @@
 import * as React from 'react';
+import { ChangeEvent, useState } from 'react';
+
 import './App.css';
 import Client from './Client';
 
@@ -8,97 +10,63 @@ import ship from './ship.png';
 /* tslint:disable:max-classes-per-file */
 /* tslint:disable:no-console */
 
-// var state = {
-//   shields: {
-//     enabled: false,
-//     strength: 0
-//   }
-// }
-
 const client = new Client("ws://localhost:8080");
 console.log(client.state);
-
 
 interface IShipProps {
   shieldLevel: number;
 }
 
-interface IAppState {
-  enabled: boolean;
-  frequency: number;
-  shieldLevel: number;
+function Ship({shieldLevel}: IShipProps) {
+    return (
+        <div className="ship">
+            <img src={ship} />
+            <img className="shield-overlay" src={shipShield} style={{opacity: shieldLevel}} />
+        </div>
+    );
 }
 
-class Ship extends React.Component<IShipProps> {
-  public render() {
-    return (
-      <div className="ship">
-        <img src={ship}/>
-        <img className="shield-overlay" src={shipShield} style={{opacity: this.props.shieldLevel}}/>
-      </div>
-    );
-  }
-}
+function App() {
+    const [enabled, setEnabled] = useState(false);
+    const [frequency, setFrequency] = useState(125);
+    const [shieldLevel, setShieldLevel] = useState(0.5);
 
-class App extends React.Component<any, IAppState> {
-  constructor(props: any) {
-    super(props);
-
-    this.state = {
-      enabled: false,
-      frequency: 125,
-      shieldLevel: 0.5
-    };
-
-    this.updateFrequency = this.updateFrequency.bind(this);
-    this.toggleEnabled = this.toggleEnabled.bind(this);
-  }
-
-  public render() {
-    return (
-      <div>
-        <div className="ship-container">
-          <Ship shieldLevel={this.state.enabled ? this.state.shieldLevel : 0}/>
-        </div>
-        <div className="shield-controls">
-            <p>
-              <button onClick={this.toggleEnabled}>{this.state.enabled ? "Deactive" : "Activate"}</button>
-            </p>
-
-            <p>
-              Shield Level: {this.state.enabled ? this.state.shieldLevel : "Disabled"}
-
-            </p>
-            <p>
-              <label htmlFor="range">Frequency</label>
-              <input id="frequency" name="range" min={50} max={200} type="range" onChange={this.updateFrequency}/>
-              {this.state.frequency} Value MHz
-            </p>
-        </div>
-      </div>
-    );
-  }
-
-  private toggleEnabled() {
-    console.log('activate');
-    this.setState({
-      ...this.state,
-      enabled: !this.state.enabled
-    });
-  }
-
-  private updateFrequency() {
-    const element = document.querySelector('#frequency');
-
-    if (!(element instanceof HTMLInputElement)) {
-      return;
+    function toggleEnabled() {
+        setEnabled(!enabled);
     }
 
-    this.setState({
-      ...this.state,
-      frequency: parseInt(element.value, 10)
-    });
-  }
+    function updateFrequency(event: ChangeEvent<HTMLInputElement>) {
+        setFrequency(event.currentTarget.valueAsNumber);
+    }
+
+    function updateShieldLevel(event: ChangeEvent<HTMLInputElement>) {
+        setShieldLevel(event.currentTarget.valueAsNumber);
+    }
+
+    return (
+        <div>
+            <div className="ship-container">
+                <Ship shieldLevel={enabled ? shieldLevel : 0} />
+            </div>
+            <div className="shield-controls">
+                <p>
+                    <button onClick={toggleEnabled}>{enabled ? "Deactivate" : "Activate"}</button>
+                </p>
+
+                <p>
+                    <label htmlFor="range">Frequency</label>
+                    <input id="frequency" name="range" min={50} max={200} type="range" onChange={updateFrequency} />
+                    {frequency} Value MHz
+                </p>
+
+                <p>
+                    <label htmlFor="range">Shield Level</label>
+                    <input id="shield-level" name="range" min={0} max={1} step={0.01} type="range" onChange={updateShieldLevel} />
+                    {Math.round(shieldLevel*100)}%
+                </p>
+            </div>
+        </div>
+    );
 }
 
 export default App;
